949a5d8e685cc59d6414a2b48a8d02500203de5c
Zach Brownson <zach.brownson@imprev.com>
2020-01-09 07:49:58 -0800

    sample interview code to review

---
+++ zachsinterview/src/main/java/com/imprev/zachsinterview/NumericalKeyValueException.java
@@ -0,0 +1,10 @@
+package com.imprev.zachsinterview;
+
+public final class NumericalKeyValueException extends Exception {
+
+    private static final long serialVersionUID = 2938395600614414399L;
+
+    public NumericalKeyValueException(String msg) {
+        super(msg);
+    }
+}
